94aaf86e067cb3cb2f7544ac6978c34d556258b3
paul-shannon <paul.thurmond.shannon@gmail.com>
2023-07-10 08:16:53 -0700

    before merge with dbeck's Uiupgrades branch

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -948,5 +948,5 @@ def createZipFile(projectDir, projectTitle):
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8009)
 
