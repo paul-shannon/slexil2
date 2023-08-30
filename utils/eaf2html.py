d9febd27b5dd0265099de084137e261046df7f46
paul-shannon <paul.thurmond.shannon@gmail.com>
2023-08-30 10:11:56 -0700

    now accepts all text class arguments

--- utils/eaf2html.py
+++ utils/eaf2html.py
@@ -1,72 +1,80 @@
-import re
-import sys
-import slexil
-from text import *
-import importlib
-import os
-import pdb
-import tempfile
 import argparse
-#-------------------------------------------------------------------------------
-pd.set_option('display.width', 1000)
-#-------------------------------------------------------------------------------
-parser = argparse.ArgumentParser()
-parser.add_argument("--eaf", help="the ELAN XML file")
-parser.add_argument("--terms", help="grammatical categories to capitalize")
-helpText = "e.g, speech, translation, morpheme,morphemeGloss, ..."
-parser.add_argument("--tierGuide", help=helpText)
-parser.add_argument("--projectDirectory", help="where to write html file")
-parser.add_argument("--verbose", help="verbose stdout reporting", action="store_true")
-parser.add_argument("--startLine", help="optional start line")
-parser.add_argument("--endLine", help="optional end line")
-parser.add_argument("--addFontSizeControls", help="text font size +/-",
-					   action="store_true")
+import os, sys
+import unittest
+from slexil.text import Text
+import yattag  # only for indent method
+import pdb
+    
+#----------------------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(prog='toHTML.py',
+          description='creates interactive webpage from eaf xml')
+
+#parser.add_argument('-f', '--eaf')
+parser.add_argument('--eaf', type=str, required=True)
+parser.add_argument('--tierGuide', type=str, required=True)
+parser.add_argument('--terms', type=str, required=False)
+parser.add_argument('--verbose', type=str, required=False, default=False)
 parser.add_argument("--helpFile", help="optional info for about box")
-parser.add_argument("--helpButtonLabel")
-parser.add_argument("--kb", help="a knowledge base file")
-parser.add_argument("--linguistics", help="a linguistics knowledge base file")
+parser.add_argument("--helpButtonLabel", help="optional button label")
 parser.add_argument("--pageTitle", help="optional html title")
- 
+parser.add_argument('--start', type=int, required=False, default=None)
+parser.add_argument('--end', type=int, required=False, default=None)
+parser.add_argument('--webpackLinksOnly',  action="store_true")
+parser.add_argument('--fontSizeControls',  action="store_true")
+parser.add_argument('--kbFilename', required=False, default=None)
+parser.add_argument('--linguisticsFilename', required=False, default=None)
+
+linguisticsFilename = None
+
 args = parser.parse_args()
-elanXmlFilename = vars(args)["eaf"]
-grammaticalTermsFile = vars(args)["terms"]
-tierGuideFile = vars(args)["tierGuide"]
-projectDirectory = vars(args)["projectDirectory"]
-verbose = vars(args)["verbose"]
-startLine = vars(args)["startLine"]
-helpFilename = vars(args)["helpFile"]
-helpButtonLabel = vars(args)["helpButtonLabel"]
-pageTitle = vars(args)["pageTitle"]
-endLine = vars(args)["endLine"]
-kbFilename = vars(args)["kb"]
-linguisticsFilename = vars(args)["linguistics"]
+print(args)
+eaf = args.eaf
+tierGuide = args.tierGuide
+terms = args.terms
+helpFile = args.helpFile
+helpButtonName = args.helpButtonLabel
+verbose = args.verbose
+startLine = args.start
+endLine = args.end
+pageTitle = args.pageTitle
+webpackLinksOnly = args.webpackLinksOnly
+fontSizeControls = args.fontSizeControls
+kbFilename = args.kbFilename
+linguisticsFilemane = args.linguisticsFilename
+#----------------------------------------------------------------------------------------------------
+if(not os.path.isfile(eaf)):
+    print("toHTML.py error: eaf file '%s' not found" % eaf)
+    sys.exit()
 
-if(startLine != None):
-	startLine = int(startLine)
-if(endLine != None):
-	endLine = int(endLine)
-fontSizeControls = vars(args)["addFontSizeControls"]
-
-text = Text(elanXmlFilename,
-            grammaticalTermsFile,
-            tierGuideFile,
-            projectDirectory,
-            verbose,
-            fontSizeControls,
-            startLine,
-            endLine,
-            pageTitle,
-            helpFilename,
-            helpButtonLabel,
-            kbFilename,
-            linguisticsFilename)
-
-print(text.getTierSummary())
+if(not os.path.isfile(tierGuide)):
+    print("toHTML.py error: tierGuidefile '%s' not found" % tierGuide)
+    sys.exit()
+    
+if(terms and os.path.isfile(terms)):
+    print("toHTML.py error: grammaticalTerms file '%s' not found" % terms)
+    sys.exit()
+    
+projectDirectory = "./"
+text = Text(xmlFilename=eaf,
+            grammaticalTermsFile=terms,
+            tierGuideFile=tierGuide,
+            projectDirectory=projectDirectory,
+            verbose=verbose,
+            fontSizeControls = fontSizeControls,
+            startLine = startLine,
+            endLine = endLine,
+            pageTitle = pageTitle,
+            helpFilename = helpFile,
+            helpButtonLabel = "About",
+            kbFilename = kbFilename,
+            linguisticsFilename = linguisticsFilename,
+            webpackLinksOnly = webpackLinksOnly,
+            fixOverlappingTimeSegments = False)
+	
 htmlText = text.toHTML()
-htmlText_indented = indent(htmlText)
-
 filename = "index.html"
 f = open(filename, "wb")
-f.write(bytes(htmlText_indented, "utf-8"))
+f.write(bytes(htmlText, "utf-8"))
 f.close()
 print("wrote %s" % f.name)
+
