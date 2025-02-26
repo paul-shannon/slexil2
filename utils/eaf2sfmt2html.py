c4b60df7a3c17e5880b4155c7a018f54f29c68dc
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-02-26 12:08:21 -0800

    now used in tlingit (DX) conversations, with more args available from the makefile

--- utils/eaf2sfmt2html.py
+++ utils/eaf2sfmt2html.py
@@ -1,3 +1,4 @@
+import argparse
 import pdb
 import io, traceback, time
 import os, sys
@@ -18,70 +19,97 @@ path = Path(".")
 from xmlschema.validators.exceptions import XMLSchemaValidationError;
 from slexil.yamlToText import YamlToText
 
-if not len(sys.argv) == 2:
-    print("usage: python eaf2sfmt2html.py <eafFile> or <listOfEafFiles.txt>")
-    sys.exit(1)
+parser = argparse.ArgumentParser(prog='yaml2html.py',
+          description='creates interactive webpage from eaf xml')
 
-filename = sys.argv[1]
-assert(os.path.exists(filename))
-baseName = os.path.splitext(os.path.basename(filename))[0]
-extension = os.path.splitext(os.path.basename(filename))[1]
-if extension == '.txt':
-   with open(filename) as f:
-      eafFiles = f.read().splitlines()
-      eafFiles = [f for f in eafFiles if not f.startswith("#")]
-      #eafFiles = f.readlines()
-else:
-    eafFiles = [filename]
+parser.add_argument('--eaf', type=str, required=True)
+parser.add_argument('--html', type=str, required=True)
+parser.add_argument('--tierGuide', type=str, required=False)
+parser.add_argument("--verbose", action="store_true")
+parser.add_argument("--helpFile", help="optional info for about box")
+parser.add_argument("--helpButtonLabel", help="optional button label")
+parser.add_argument("--pageTitle", help="optional html title")
+parser.add_argument('--start', type=int, required=False, default=None)
+parser.add_argument('--end', type=int, required=False, default=None)
+parser.add_argument('--webpackLinksOnly',  action="store_true")
+parser.add_argument('--fontSizeControls',  action="store_true")
+parser.add_argument('--kbFilename', required=False, default=None)
+parser.add_argument('--linguisticsFilename', required=False, default=None)
+parser.add_argument('--fixOverlappingTimeSegments', action="store_true")
+parser.add_argument('--toolTips', action="store_true")
+parser.add_argument('--outputDir', default="./")
 
-for eafFile in eafFiles:
-   try:
-      print("\n\n-----------------------------------------------------------")
-      print("    %s" % eafFile)
-      baseName = os.path.splitext(os.path.basename(eafFile))[0]
-      tats = extractAllTimeAlignedTierIDs(eafFile)
-      tatsString = ", ".join(map(str, tats))
-      print("tats found: %s" % tatsString)
-      if len(tats) != 1:
-          continue
-      p = EafParser(eafFile, verbose=False, fixOverlappingTimeSegments=False)
-      p.run()
-      print("-----------------------------------------------------------------")
-      print(p.getRichTierTables()[0])
-      print()
-      print(p.getRichTierTables()[1])
-      print("-----------------------------------------------------------------")
-      tbl = p.getTierTable()
-      text = p.toSFMT("title", "speaker", "transcriber")
+args = parser.parse_args()
+print(args)
+eaf = args.eaf
+htmlFileName = args.html
+tierGuide = args.tierGuide
+#grammaticalTerms = args.terms
+helpFile = args.helpFile
+helpButtonLabel = args.helpButtonLabel
+verbose = args.verbose
+startLine = args.start
+endLine = args.end
+pageTitle = args.pageTitle
+webpackLinksOnly = args.webpackLinksOnly
+fontSizeControls = args.fontSizeControls
+kbFilename = args.kbFilename
+linguisticsFilename = args.linguisticsFilename
+fixOverlappingTimeSegments = args.fixOverlappingTimeSegments
+useTooltips = args.toolTips
+outputDir = args.outputDir
+
+baseName = os.path.splitext(os.path.basename(eaf))[0]
+try:
+   print("\n\n-----------------------------------------------------------")
+   print("    %s" % eaf)
+   baseName = os.path.splitext(os.path.basename(eaf))[0]
+   tats = extractAllTimeAlignedTierIDs(eaf)
+   tatsString = ", ".join(map(str, tats))
+   print("tats found: %s" % tatsString)
+   if len(tats) != 1:
+       sys.exit(1)
+   p = EafParser(eaf, verbose=False, fixOverlappingTimeSegments=False)
+   p.run()
+   print("-----------------------------------------------------------------")
+   print(p.getRichTierTables()[0])
+   print()
+   print(p.getRichTierTables()[1])
+   print("-----------------------------------------------------------------")
+   tbl = p.getTierTable()
+   text = p.toSFMT("title", "speaker", "transcriber")
+   yamlOutFile = "%s.yaml" % baseName
+   if os.path.isdir("testResults"):
       yamlOutFile = "testResults/%s.yaml" % baseName
-      p.writeSFMT(text, yamlOutFile)
-      projectDirectory = "./"
-      text = sfmtToWebPage(yamlOutFile,
-                           grammaticalTerms=[],
-                           projectDirectory=projectDirectory,
-                           verbose = False,
-                           fontSizeControls = True,
-                           startLine = None,
-                           endLine = None,
-                           pageTitle = baseName,
-                           helpFilename = None,
-                           helpButtonLabel = None,
-                           kbFilename = None,
-                           linguisticsFilename = None,
-                           fixOverlappingTimeSegments = False,
-                           webpackLinksOnly=False,
-                           useTooltips=False)
-      htmlText = text.toHTML()
-      htmlFileName = "testResults/%s.html" % baseName
-      print("--- writing html file for at %s" % htmlFileName)
-      with open(htmlFileName, "w") as file:
-          file.write(htmlText)
-   except Exception as e:
-      print("exception occurred")
-      file = io.StringIO()
-      exceptionString = traceback.format_exception_only(None, e)
-      tracebackString = traceback.print_exception(e, file=file)
-      print(file.getvalue().rstrip())
-      print()
-      print(exceptionString)
+   p.writeSFMT(text, yamlOutFile)
+   projectDirectory = "./"
+   text = sfmtToWebPage(yamlOutFile,
+                        grammaticalTerms=[],
+                        projectDirectory=projectDirectory,
+                        verbose = False,
+                        fontSizeControls = True,
+                        startLine = None,
+                        endLine = None,
+                        pageTitle = pageTitle,
+                        helpFilename = helpFile,
+                        helpButtonLabel = helpButtonLabel,
+                        kbFilename = kbFilename,
+                        linguisticsFilename = linguisticsFilename,
+                        fixOverlappingTimeSegments = False,
+                        webpackLinksOnly=False,
+                        useTooltips=False)
+   htmlText = text.toHTML()
+   if os.path.isdir("testResults"):
+      htmlFileName = "testResults/%s.html" % htmlFileName
+   print("--- writing html file for at %s" % htmlFileName)
+   with open(htmlFileName, "w") as file:
+       file.write(htmlText)
+except Exception as e:
+   print("exception occurred")
+   file = io.StringIO()
+   exceptionString = traceback.format_exception_only(None, e)
+   tracebackString = traceback.print_exception(e, file=file)
+   print(file.getvalue().rstrip())
+   print()
+   print(exceptionString)
 
