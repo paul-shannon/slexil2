d52600688b63ae5d9d69333a51bace9420ab6b90
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-05-11 12:06:47 -0700

    recording improved.  need to sort out recorder creation

--- explore/misc/inferno/recordPopup.js
+++ explore/misc/inferno/recordPopup.js
@@ -1,20 +1,29 @@
-<div id="recordingPopup" style="display: none"></div>
 
 <script type="module">
+
+var player, recorder, wavesurfer;
+
 import WaveSurfer from 'https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js'
-import RecordPlugin from 'https://unpkg.com/wavesurfer.js/dist/plugins/record.esm.js'
+import RecordPlugin from 'https://unpkg.com/wavesurfer.js@7/dist/plugins/record.esm.js'
 
 var recorder, wavesurfer;
 wavesurfer = WaveSurfer.create({container: "#recorderDiv"})
-recorder = wavesurfer.registerPlugin(
-    RecordPlugin.create({
-       renderRecordedAudio: true,
-       scrollingWaveform: true,
-       continuousWaveform: true,
-       continuousWaveformDuration: 30, // optional
-    }))
+
+//recorder = wavesurfer.registerPlugin(
+//    RecordPlugin.create({
+//       renderRecordedAudio: true,
+//       scrollingWaveform: true,
+//       continuousWaveform: true,
+//       continuousWaveformDuration: 30, // optional
+//    }))
 
 
+//--------------------------------------------------------------------------------
+function loadURL(url)
+{
+   player.load(url)
+
+} // loadURL
 //--------------------------------------------------------------------------------
 function createRecorder(containerID, buttonID, endOfRecordingFunction)
 {
@@ -28,10 +37,12 @@ function createRecorder(containerID, buttonID, endOfRecordingFunction)
   
   recorder = wavesurfer.registerPlugin(
     RecordPlugin.create({
-       renderRecordedAudio: true,
+       renderRecordedAudio: false,
+       audioRate: 1, // Don't modify playback rate
+       bufferSize: 512, // Try smaller values (1024, 512)
        scrollingWaveform: true,
-       continuousWaveform: true,
-       continuousWaveformDuration: 30, // optional
+       continuousWaveform: false,
+      // continuousWaveformDuration: 30, // optional
        }),
        ) // registerPlugin
 
@@ -43,9 +54,9 @@ function createRecorder(containerID, buttonID, endOfRecordingFunction)
      })
 
   RecordPlugin.getAvailableAudioDevices().then((devices) => {
-      console.log("device promise fulfilled");
+      console.log("audio device promise fulfilled");
       devices.forEach((device) => {
-          console.log(device.deviceId)
+          console.log("device id: " + device.deviceId)
       })})
 
     return(recorder);
@@ -56,33 +67,96 @@ function recordingEndHandler(recordedUrl)
 {
    console.log(" ---- recording ended, seen by recorderModuleTest.html")
    console.log(" ---- url: " + recordedUrl)
-}
+   console.log("children: " +    $("#recorderDiv").children().length)
+   // $("#recorderDiv").children()[0].remove()
+   $("#recorderDiv").hide()
+   $("#playerDiv").show()
+   $("#playRecordingButton").show()
+   if(player == undefined){
+      player = createPlayer(recordedUrl, "#playerDiv","#playRecordingButton")
+      window.player = player
+      console.log("player")
+      console.log(player)
+      }
+   else{
+      loadURL(recordedUrl)
+      }
+
+} // recordingEndHandler
+//--------------------------------------------------------------------------------
+function createPlayer(url, containerID, buttonID)
+{
+  if(player){
+     console.log("--- destroying player")
+     player.destroy();
+     }
+
+   player = WaveSurfer.create({
+      container: containerID,
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      url: url
+      })
+
+    window.player = player;
+    player.on('finish', function() {
+       console.log("playback finished");
+       $(buttonID).text("Play")
+       })
+
+   $(buttonID).on("click", function(e){
+       e.stopImmediatePropagation(); // not sure why this is needed
+       let buttonLabel = $(buttonID).text()
+       console.log("--- play/pause button click, current label: " + buttonLabel)
+       if(buttonLabel == "Play"){
+          $(buttonID).text("Pause")
+          //console.log(player)           
+          player.play();
+          }
+       else{
+          console.log("--- pausing play");
+          player.pause()
+          $(buttonID).text("Play")
+          }
+       }) // on button click
+
+  return(player)
+
+} // createPlayer
 //--------------------------------------------------------------------------------
 $(document).ready(function() {
-   $('#buttonsAndRecorderDiv').dialog({autoOpen: false,
-                                       title: 'Record Your Voice',
-                                       width: 800
-                                       });
+
+   $('#recordingPopup').dialog({autoOpen: false,
+                               title: 'Record Your Voice',
+                               width: 800,
+                               height: 400
+                               });
+
    $("#openRecordDialogButton").on('click', function(){
        let newStatus = 'open'
-       if($("#buttonsAndRecorderDiv").is(":visible")){
+       if($("#recordingPopup").is(":visible")){
           newStatus = 'close'
           }
-       $("#buttonsAndRecorderDiv").dialog(newStatus);
+       $("#recordingPopup").dialog(newStatus);
        });
 
-    $("#recordButton").on("click", function(){
+   $("#recordButton").on("click", function(){
        console.log("record!")
        let incomingState = $("#recordButton").text()
        console.log(" incomingState: " + incomingState);
        if(incomingState == "Record"){
+          $("#recorderDiv").empty()
           $("#recorderDiv").show()
           $("#playerDiv").hide()
           $("#recordButton").text("Stop")
-          recorder = createRecorder("#recorderDiv", "#recordButton",
-                                    recordingEndHandler);   
-          let deviceId = "default"; // $('#microphone-selector').find(":selected").val()
-          console.log("mic deviceId: " + deviceId)
+
+          debugger;
+          if (typeof(recorder) == "undefined"){
+             recorder = createRecorder("#recorderDiv", "#recordButton",
+                                       recordingEndHandler);   
+             let deviceId = "default"; // $('#microphone-selector').find(":selected").val()
+             console.log("mic deviceId: " + deviceId)
+             }
           recorder.startRecording("default").then(() => {
              console.log("recording started, from promise")
              });
