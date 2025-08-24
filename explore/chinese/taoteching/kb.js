42500c55b29f4777f4a6d405d0ec361e5609bd56
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-08-23 17:54:31 -0700

    latest

---
+++ explore/chinese/taoteching/kb.js
@@ -0,0 +1,171 @@
+kb = {
+
+"xin":
+`
+**地: di**
+
+Phonosemantic compound. 土 represents the meaning and 也 represents the sound.
+
+- radical: earth 土
+- Parts: 土 也
+- earth
+- ground
+- field
+- place
+- land
+`,
+
+
+
+心: heart, mind, heart-mind (mindfulness)
+- heart
+- mind
+- heart-mind
+- mindfulness
+`,
+
+
+"zhēng":
+`
+爭: zhēng
+
+- to fight, compete, strive for
+- to rise
+- excel
+- lofty
+- to strive for
+- to vie for
+- to argue or debate
+
+`,
+
+
+"bù":
+`
+不: bù
+
+- not, no
+- primarily used to negate verbs, indicating the absence of an action or state.
+
+`,
+
+
+"ér":
+`
+而: ér
+
+- and
+- but
+- however
+- yet
+
+`,
+
+"wù":
+`
+物 : wù
+
+- thing
+- object
+- matter
+- material
+- awareness, consciousness, or spiritual enlightenment in the Chinese
+  folk religion and Chinese Buddhism
+`,
+
+"wàn":
+`
+萬: wàn
+
+- ten thousand
+- a great number
+- multitude
+- everything
+- all
+
+`,
+
+
+"lì":
+`
+利: lì
+
+- sharp
+- favorable
+- advantage
+- benefit
+- profit
+- interest
+- to do good to
+- to benefit
+`,
+
+
+"shuǐ":
+`
+水: shuǐ
+
+- water
+- river
+- liquid
+- beverage
+- additional charges or income
+- (of clothes) classifier for number of washes
+
+`,
+
+"ruò":
+`
+若: ruò
+
+- if: often used as a conjunction to introduce a conditional clause, similar to "if" in English. For example, "若有 (ruò yǒu)": "if there is".
+- like, as if: indicates a comparison or resemblance. 
+- to seem, to appear: suggests something seems a certain way. 
+`,
+
+"shàng":
+`
+上: shàng
+
+Originally a small line or dot above a longer line, indicating
+"up". The vertical line was added to distinguish the character from 二
+(two). Opposite of 下.
+
+- on top
+- upon
+- above
+- upper
+- previous
+- first (of multiple parts)
+- to climb
+- to get onto
+- to go up
+- to attend (class or university)
+
+`,
+
+
+"shàn":
+`
+善: shàn
+
+Composed of two 言 (speech) components and 羊 (sheep). In ancient
+China sheep (羊) were associated with goodness and beauty, which is
+why 羊 is a component in characters like 美 (beautiful), 善
+(benevolent), and 義 (justice).
+
+- the traditional sense of moral virtue and  goodness
+- take to be good, like.
+- good person
+- strong point, merit.
+- perfect
+- gentle, kind
+- friendly
+- good at doing
+- improve, make good
+- control, regulate.
+- food
+`
+
+}
+
