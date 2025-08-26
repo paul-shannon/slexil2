695c834e6c3048fb2c54d1de40d73236cfa7eb62
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-08-25 19:02:03 -0700

    lesson 28, up to line 12

---
+++ explore/lushootseed/grammars/v2/kb2.js
@@ -0,0 +1,66 @@
+kb = {
+
+"tu(u)k̓ʷəɬalikʷ čəd ʔə ti kupi čəda tuk̓ʷəɬdxʷ.":
+`
+**Vocabulary Comment, 28.3.7, page 3**
+
+_kupi_ is a Lushootseed word taken from English "coffee".
+
+A few elders
+prefer the word _dxʷs(h)ədalqʷuʔ_ "hot water" when referring to
+coffee or tea.
+
+`,
+
+
+"xʷiʔ gʷədsqəɬcid.  cick̓ʷ čəxʷ ƛ̓uʔitut ʔə tə tib.  xʷiʔ gʷ(ə)adsqəɬ.":
+`
+**Grammar Note 28.2, page 3**
+
+_ƛ̓u-_ is a prefix meaning "habiutally".  When both _ƛ̓u-_ and _tu-_
+(see section 10.7) or _ƛ̓u-_ and _ɬu-_ (11.7) precede the same action word,
+_ƛ̓u-_ is first.
+
+`,
+
+"ʔi.  ʔuqəɬdubš čəxʷ. gʷəƛ̓əladəxʷ.":
+`
+**Footnote 1, p1**
+
+In Southern Lushootseed the equivalent expression is _ƛ̓əlusəbəxʷ_.
+`
+,
+
+
+"ʔuqəɬdubicid čəd (ʔ)u.":
+`
+**Grammar Note 28.1, page 3**
+
+Some words can take both _-dxʷ_ and _-d_ endings.  Note the difference in
+meaning between
+
+- _qəɬcid_: wake you up deliberately
+- _qəɬdubicid_: happen to wake you up
+
+and between
+
+- _k̓ʷəɬəd_: pour it out
+- _k̓ʷəɬdxʷ_: spill it
+
+In these two examples it would seem that _-dxʷ_ (_-du_) means
+accidentally.
+
+However, the following pairs of words show that it actually means
+_someone lacks full control over what he is doing_:
+
+- _tuhuydxʷ čəd_: I solved it, I pieced it together.
+- _thuhyud čəd_: I made it, I finished it.
+
+- _tuƛ̓aldxʷ čəd_: I managed to get i on (e.g., a tight boot)
+_ _tuɬalš čəd_: I put it on.
+
+`,
+
+
+
+}
