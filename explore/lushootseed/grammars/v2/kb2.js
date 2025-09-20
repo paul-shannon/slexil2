6dc04631c519be0cc64dea5b38b48d8fb21c76c8
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-09-19 20:41:43 -0700

    latest

--- explore/lushootseed/grammars/v2/kb2.js
+++ explore/lushootseed/grammars/v2/kb2.js
@@ -26,15 +26,51 @@ precede the same action word, _ƛ̓u-_ is first.
 
 "ʔi.  ʔuqəɬdubš čəxʷ. gʷəƛ̓əladəxʷ.":
 `
+**Vocabulary Notes**
+
+- _ʔi_: yes.  LD 14.
+- ʔuqəɬdubš
+     - _ʔu_: perfective prefix (action viewed as a whole).  LD 19
+     - _qəɬ_: wake up.  LD 176
+     - _dubš_ from _dxʷ-bš_
+         - _-dxʷ_: diminished control.  LD 86.
+         - _-bš_: me, suffix indicating first person object of the verb. see _-š_ LD 207.
+- _čəxʷ_: you (singular).  LD 65
+- _gʷəƛ̓əladəxʷ_
+     - _gʷə_: if, maybe, might [subjunctive prefix]
+     - _ƛ̓əlad_: stop, be still.  LD 153
+     - _-əxʷ_: now.  see _-axʷ_, LD 30
+
 **Footnote 1, p1**
 
-In Southern Lushootseed the equivalent expression is _ƛ̓əlusəbəxʷ_.
+In Southern Lushootseed _ƛ̓əlusəbəxʷ_ is the equivalent expression for
+_gʷəƛ̓əladəxʷ_.
+
 `
 ,
 
 
 "ʔuqəɬdubicid čəd (ʔ)u.":
 `
+**Vocabulary Notes**
+
+___ʔuqəɬdubicid___
+- ___ʔu-___ &nbsp; an action is viewed as a whole, in its entirety, rather than
+   highlighting the details of its process. LD 19, third entry.
+- ___qəɬ___ &nbsp; wake up; regain consciousness, come to.  LD 176
+- ___-du-___ &nbsp; indicates diminished control, that the subject (the speaker)
+  did not have complete control of their act
+     - _dxʷ_ is the standard form, which often
+      becomes _du-_ when followed (as here) by other suffixes.
+- ___-bicid___ &nbsp; suffix indicating second person singular object of the verb, "you"
+     
+
+___čəd___
+   - I, me.  LD 62.
+   
+___ʔu___
+   - question marker.  LD 19, first entry.
+
 **Grammar Note 28.1, page 3**
 
 Some words can take both _-dxʷ_ and _-d_ endings.
---
+++ explore/lushootseed/grammars/v2/linguisticTerms2.js
@@ -0,0 +1,41 @@
+linguistics = {
+
+"aspect":
+
+`
+**Aspect Marking in Lushootseed via Prefixes**
+
+These prefixes indicate how an action, event or state extends over time.
+Lushootseed has three primary (simple) aspectual prefixes
+
+- __Ø-__ imperfective
+- __ʔu-__ perfective
+- __ʔas-__ stative
+- __lə-__ progressive
+- __ləs-__ progressive state (__lə__ + __ʔas__)
+- __ləcu-__ continuous  (__lə__ + __ʔu__)
+
+**Imperfective**
+
+This aspect, expressed in speech by the <b>absence</b> of ___ʔu-___
+and ___ʔas-___, indicates that the action of the very is ongoing,
+continuous, habitual, or as unfolding over time, rather than as
+completed events.  An example of this is ___haydxʷ___.  Note that
+neither  ___ʔu-___ or ___ʔas-___ is present.  The __knowing__ referred
+to here extends over time.  Hence, imperfective.
+
+<pre>
+haydxʷ əlgʷəʔ stubš tiʔiɬ
+Ø–hay–dxʷ əlgʷəʔ stubš tiʔiɬ
+IMPF–known–DC PL man DIST
+‘they know that he is a man’
+</pre>
+[DS Star Child, line 131]
+
+**Other Clitics**
+
+- __bə-__ additive
+
+`,
+
+}
