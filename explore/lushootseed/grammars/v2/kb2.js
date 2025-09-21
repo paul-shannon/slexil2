2b2ded56b52f232114bc8f57ed233f3cf11b928a
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-09-21 15:10:21 -0700

    latest, through 28.2

--- explore/lushootseed/grammars/v2/kb2.js
+++ explore/lushootseed/grammars/v2/kb2.js
@@ -1,75 +1,31 @@
 kb = {
 
-"tu(u)k̓ʷəɬalikʷ čəd ʔə ti kupi čəda tuk̓ʷəɬdxʷ.":
-`
-**Vocabulary Comment, 28.3.7, page 3**
-
-_kupi_ is a Lushootseed word taken from English "coffee".
-
-A few elders
-prefer the word _dxʷs(h)ədalqʷuʔ_ "hot water" when referring to
-coffee or tea.
-
-`,
-
-
-"xʷiʔ gʷədsqəɬcid.  cick̓ʷ čəxʷ ƛ̓uʔitut ʔə tə tib.  xʷiʔ gʷ(ə)adsqəɬ.":
-`
-**Grammar Note 28.2, page 3**
-
-_ƛ̓u-_ is a prefix meaning "habitually".
-
-When both _ƛ̓u-_ and _tu-_ (see section 10.7) or _ƛ̓u-_ and _ɬu-_ (11.7)
-precede the same action word, _ƛ̓u-_ is first.
-
-`,
-
-"ʔi.  ʔuqəɬdubš čəxʷ. gʷəƛ̓əladəxʷ.":
-`
-**Vocabulary Notes**
-
-- _ʔi_: yes.  LD 14.
-- ʔuqəɬdubš
-     - _ʔu_: perfective prefix (action viewed as a whole).  LD 19
-     - _qəɬ_: wake up.  LD 176
-     - _dubš_ from _dxʷ-bš_
-         - _-dxʷ_: diminished control.  LD 86.
-         - _-bš_: me, suffix indicating first person object of the verb. see _-š_ LD 207.
-- _čəxʷ_: you (singular).  LD 65
-- _gʷəƛ̓əladəxʷ_
-     - _gʷə_: if, maybe, might [subjunctive prefix]
-     - _ƛ̓əlad_: stop, be still.  LD 153
-     - _-əxʷ_: now.  see _-axʷ_, LD 30
-
-**Footnote 1, p1**
-
-In Southern Lushootseed _ƛ̓əlusəbəxʷ_ is the equivalent expression for
-_gʷəƛ̓əladəxʷ_.
-
-`
-,
-
+//----------------------- 
+// section 28.1, line 1
+//-----------------------
 
 "ʔuqəɬdubicid čəd (ʔ)u.":
 `
 **Vocabulary Notes**
 
-___ʔuqəɬdubicid___
-- ___ʔu-___ &nbsp; an action is viewed as a whole, in its entirety, rather than
-   highlighting the details of its process. LD 19, third entry.
-- ___qəɬ___ &nbsp; wake up; regain consciousness, come to.  LD 176
-- ___-du-___ &nbsp; indicates diminished control, that the subject (the speaker)
-  did not have complete control of their act
-     - _dxʷ_ is the standard form, which often
-      becomes _du-_ when followed (as here) by other suffixes.
-- ___-bicid___ &nbsp; suffix indicating second person singular object of the verb, "you"
+<ul>
+  <li><b> ʔuqəɬdubicid</b>
+   <ul>
+   <li><b> ʔu-</b>:  an action is viewed as a whole, in its entirety, rather than
+   highlighting the details, stages, or ongoing nature. LD 19, third entry.
+   <li><b> qəɬ</b>: wake up; regain consciousness, come to.  LD 176
+   <li><b> -du-</b>: indicates diminished control: the subject (the speaker)
+          did not have complete control of the act.  <b>dxʷ</b>
+          is the standard form, which often becomes <b>du</b> when
+          followed (as here) by other suffixes.
+   <li><b>-bicid </b>: suffix indicating second person singular object of the verb, "you"
      
+</ul>
 
-___čəd___
-   - I, me.  LD 62.
-   
-___ʔu___
-   - question marker.  LD 19, first entry.
+   <li><b> čəd </b>:  I, me.  [LD 62]
+   <li><b>ʔu </b>: question marker.  [LD 19]
+
+</ul>
 
 **Grammar Note 28.1, page 3**
 
@@ -103,6 +59,138 @@ _someone lacks full control over what he is doing_:
 _ _tuɬalš čəd_: I put it on.
 
 `,
+
+//----------------------- 
+// section 28.1, line 2
+//-----------------------
+
+"ʔi.  ʔuqəɬdubš čəxʷ. gʷəƛ̓əladəxʷ.":
+`
+**Vocabulary Notes**
+
+<ul>
+   <li><b>ʔi</b>: yes.  [LD 14]
+   <li><b>ʔuqəɬdubš</b>
+   <ul>
+      <li><b>ʔu</b>: perfective prefix (action viewed as a whole).  [LD 19]
+      <li><b>qəɬ</b>: wake up.  [LD 176]
+      <li><b>dubš</b> from <b>dxʷ-bš</b>
+      <ul>
+         <li><b>-dxʷ_</b> diminished control.  [LD 86]
+         <li><b>-bš</b>: me, suffix indicating first person object of the verb. see _-š_ LD 207.
+      </ul>
+    </ul>
+   <li><b>čəxʷ</b>:  you (singular).  LD 65
+   <li><b>gʷəƛ̓əladəxʷ</b>
+   <ul>
+      <li><b>gʷə</b>: if, maybe, might, doubtful, hoped for [subjunctive prefix]
+      <li><b>ƛ̓əlad</b>:  stop, be still.  LD 153
+      <li><b>-əxʷ</b>: now.  see _-axʷ_, LD 30
+    </ul>
+</ul>
+
+
+**Footnote 1, p1**
+
+In Southern Lushootseed _ƛ̓əlusəbəxʷ_ is the equivalent expression for
+_gʷəƛ̓əladəxʷ_.
+
+`,
+
+//----------------------- 
+// section 28.2, line 3
+//-----------------------
+
+"gʷəqəɬc čəxʷ (ʔ)u [ʔal kʷi] ɬup dadatu.":
+
+`
+**Vocabulary Notes**
+
+<ul>
+   <li><b>gʷəqəɬc</b>
+   <ul>
+     <li><b>gʷə</b>: if, maybe, might, doubtful, hoped for. [LD 98]
+     <li><b>qəɬ</b>: wake up.  [LD 176]
+     <li><b>-c</b>: the verb ("wake up") has a goal [LD 43]
+   </ul>
+   <li><b>čəxʷ</b>:  you (singular).  [LD 65]
+   <li><b>ʔu</b>: question marker. [LD 19]
+   <li><b>ʔal</b>: on, along, through, in, at; when
+         Be located at or by some specific point in time or space,
+         location with reference to something named.  [LD 4]
+   <li><b>kʷi</b>: remote
+   <li><b>ɬup</b>:  early morning, dawn. [LD 48]
+   <li><b>dadatu</b>: morning, tomorrow, early. [LD 77]
+
+</ul>
+
+`,
+
+
+"tu(u)k̓ʷəɬalikʷ čəd ʔə ti kupi čəda tuk̓ʷəɬdxʷ.":
+`
+**Vocabulary Comment, 28.3.7, page 3**
+
+_kupi_ is a Lushootseed word taken from English "coffee".
+
+A few elders
+prefer the word _dxʷs(h)ədalqʷuʔ_ "hot water" when referring to
+coffee or tea.
+
+`,
+
+//----------------------- 
+// section 28.2, line 4
+//-----------------------
+
+"xʷiʔ gʷədsqəɬcid.  cick̓ʷ čəxʷ ƛ̓uʔitut ʔə tə tib.  xʷiʔ gʷ(ə)adsqəɬ.":
+`
+**Vocabulary Notes**
+
+<ul>
+   <li><b>xʷiʔ</b>: no, not, nothing! [LD 252]
+   <li><b>gʷədsqəɬcid</b>:
+     <ul>
+       <li><b>gʷə</b>: if, maybe, might, doubtful, hoped for. [LD 98]
+       <li><b>ds</b>: <i>unsure, might mean "will not", see gʷədsəxʷsʔitutəb </i>  [LD 18]
+       <li><b>qəɬ</b>: wake up.  [LD 176]
+       <li><b>cid</b>: you (singular) [LD 46]
+     </ul>
+<li><b>cick̓ʷ</b>: very [LD 46]
+<li><b>čəxʷ</b>: you (singular) [LD 65]
+<li><b>ƛ̓uʔitut</b>:
+   <ul>
+    <li><b>ƛ̓u</b>: habitual action or state, generally, usually [LD 155]
+     <li><b>ʔitut</b>: sleep [LD 18]
+   </ul>
+<li><b>ʔə</b>: (a linking particle) about, as, in, of, on, into, onto, when, while [LD 9]
+<li><b>tə</b>: the, a [LD 219]
+<li><b>tib</b>: do something requring much physical effort; be strong [LD 225]
+<li><b>xʷiʔ</b>: no, not, nothing! [LD 252]
+<li><b>gʷ(ə)adsqəɬ</b>:
+   <ul>
+      <li><b>gʷə</b>: if, maybe, might, doubtful, hoped for. [LD 98]
+      <li><b>ad </b>: your (second person singular possessive) [LD 25]
+      <li><b>s </b>:  nominalizer [LD 199]
+      <li><b>qəɬ</b>: wake up.  [LD 176]
+   </ul>
+
+</ul>
+
+
+**Grammar Note 28.2, page 3**
+
+_ƛ̓u-_ is a prefix meaning "habitually".
+
+When both _ƛ̓u-_ and _tu-_ (see section 10.7) or _ƛ̓u-_ and _ɬu-_ (11.7)
+precede the same action word, _ƛ̓u-_ is first.
+
+`,
+
+
+
+
+
 
 
 
