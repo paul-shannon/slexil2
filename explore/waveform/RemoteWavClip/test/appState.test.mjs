3eaa36fe2e3196bed1ff42957652895c12202d5e
paul-shannon <paul.thurmond.shannon@gmail.com>
2026-08-12 13:05:25 -0700

    two classes tested

---
+++ explore/waveform/RemoteWavClip/test/appState.test.mjs
@@ -0,0 +1,54 @@
+import test from 'node:test';
+//import assert from 'node:assert/strict';
+import assert from 'node:assert';
+import State from '../appState.mjs';
+
+//--------------------------------------------------------------------------------
+test('constructor simply creates the internal data structure, currently a Map', () => {
+
+  console.log("--- testing appState constructor");
+
+  const state = new State()
+  assert.equal(state.size(), 0)
+  });
+
+//--------------------------------------------------------------------------------
+test('setters and getters', () => {
+
+  console.log("--- testing appState setters and getters");
+
+  const state = new State()
+  assert.equal(state.size(), 0)
+  state.set("x", 1)
+  state.set("y", "2")
+  state.set("z", [1,2,3])
+  assert.equal(state.size(),3)
+
+  const x = state.get("x")
+  assert.equal(x, 1)
+
+  const y = state.get("y")
+  assert.equal(y, "2")
+
+  const z = state.get("z")
+  assert.deepEqual(z, [1,2,3])
+
+  });
+
+//--------------------------------------------------------------------------------
+test('keys', () => {
+
+  console.log("--- testing appState keys");
+  
+  const state = new State()
+  assert.equal(state.size(), 0)
+  state.set("x", 1)
+  state.set("z", [1,2,3])
+  state.set("y", "2")
+
+  assert.deepEqual(state.keys(), ["x", "z", "y"])
+  assert.equal(state.hasKey("x"), true)
+  assert.equal(state.hasKey("xyz"), false)
+
+});
+//--------------------------------------------------------------------------------
